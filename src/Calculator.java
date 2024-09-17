0a78055213aeee9b0b88d6e3c1487e1563ee6428
Алексей <balko-aleksey@yandex.ru>
2024-09-17 21:54:27 +0700

    completed

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,25 @@
 public class Calculator {
-
+    double a;
+    double b;
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public double summ(int a, int b) {
+        double summa = a + b;
+        return summa;
+    }
+    public double difference (int a, int b) {
+        double minus = a - b;
+        return minus;
+    }
+    public void multiplication() {
+            double multiply = a * b;
+        System.out.println("Произведение а  и б  = " + multiply);
+    }
+    public void division() {
+        double div = a / b;
+        System.out.println("Частное а  и б  = " + div);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
-
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Calculator calculator = new Calculator(5, 3);
+        double summa = calculator.summ(2, 5);
+        double minus = calculator.difference(2, 5);
+        System.out.println("Сумма а и б = " + summa);
+        System.out.println("Разность а и б = " + minus);
+        calculator.multiplication();
+        calculator.division();
     }
 }
